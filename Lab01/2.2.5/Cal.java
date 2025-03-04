48458bc79e362c75911d0e1803ae402d215a2488
tvd20235697 <duy9101015@gmail.com>
2025-03-04 15:52:12 +0700

    Update Cal.java

--- Lab01/2.2.5/Cal.java
+++ Lab01/2.2.5/Cal.java
@@ -4,17 +4,30 @@ public class Cal {
     public static void main(String[] args) {
         String strnum1;
         String strnum2;
-        String strnotification = "Bạn vừa nhập: ";
         strnum1 = JOptionPane.showInputDialog(null, "Vui lòng nhập số thứ nhất: ", "Nhập số thứ nhất", JOptionPane.INFORMATION_MESSAGE);
         strnum2 = JOptionPane.showInputDialog(null, "Vui lòng nhập số thứ hai: ", "Nhập số thứ hai", JOptionPane.INFORMATION_MESSAGE);
-        strnotification += strnum1 + " và " + strnum2;
-        JOptionPane.showMessageDialog(null, strnotification, "Hiển thị hai số", JOptionPane.INFORMATION_MESSAGE);
         double a=Double.parseDouble(strnum1);
         double b=Double.parseDouble(strnum2);
-        System.out.println("a+b="+(a+b));
-        System.out.println("a-b="+(a-b));
-        System.out.println("a*b="+(a*b));
-        System.out.println("a/b="+(a/b));
+        String s="1.Tinh tong\n2.Tinh hieu\n3.Tinh tich\n4.Tinh thuong";
+        String strnum3;
+        strnum3=JOptionPane.showInputDialog(null,s,"Option",JOptionPane.INFORMATION_MESSAGE);
+        double c=Double.parseDouble(strnum3);
+        if(c==1)
+        {
+            JOptionPane.showMessageDialog(null,"a+b="+(a+b),"Result",JOptionPane.INFORMATION_MESSAGE);
+        }
+        if(c==2)
+        {
+            JOptionPane.showMessageDialog(null,"a+b="+(a+b),"Result",JOptionPane.INFORMATION_MESSAGE);
+        }
+        if(c==3)
+        {
+            JOptionPane.showMessageDialog(null,"a+b="+(a+b),"Result",JOptionPane.INFORMATION_MESSAGE);
+        }
+        if(c==4)
+        {
+            JOptionPane.showMessageDialog(null,"a+b="+(a+b),"Result",JOptionPane.INFORMATION_MESSAGE);
+        }
         System.exit(0);
     }
 }
